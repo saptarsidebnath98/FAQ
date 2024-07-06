4531320ba894174ad5fc2d671a2f0fe0eb9785cd
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2024-07-06 13:04:50 +0530

    setting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen w-screen relative">
+      <div className="h-1/3 w-full bg-background-pattern bg-cover bg-center bg-no-repeat">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="h-2/3 w-full bg-LightPink">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="bg-white bg-opacity-100 h-3/4 w-1/3 p-10 rounded-lg shadow-lg text-center">
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'White': 'hsl(0, 0%, 100%)',
+        'LightPink': 'hsl(275, 100%, 97%)',
+        'GrayishPurple': 'hsl(292, 16%, 49%)',
+        'DarkPurple': 'hsl(292, 42%, 14%)'
+      },
+      backgroundImage: {
+        'background-pattern': "url('/images/background-pattern-desktop.svg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
